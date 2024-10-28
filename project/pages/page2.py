5c91c38e6bbbb2dc1d493f17dec1ed60866a6690
MinTuanLuong <tuanlm.t1.1821@gmail.com>
2024-10-28 16:45:33 +0000

    a

--- project/pages/page2.py
+++ project/pages/page2.py
@@ -7,7 +7,7 @@ def show():
     st.write("Hãy cùng tìm hiểu định nghĩa hình nón qua hoạt động dưới đây.")
     
     geogebra_url = "https://www.geogebra.org/m/qkp4se2d"
-    if st.button("Tương tác với Hình nón trên Geogebra"):
+    if st.button("Tìm hiểu định nghĩa Hình nón trên Geogebra"):
         st.write("Mở liên kết [Geogebra - Hình nón](%s)" % geogebra_url)
         st.write("Hoặc bấm vào nút bên dưới để mở trong tab mới.")
         st.markdown(f'<a href="{geogebra_url}" target="_blank"><button>Geogebra - Hình nón</button></a>', unsafe_allow_html=True)
@@ -16,7 +16,7 @@ def show():
     st.write("Hãy cùng tìm hiểu công thức tính diện tích xung quanh và thể tích của hình nón qua hoạt động dưới đây.")
 
     geogebra_url = "https://www.geogebra.org/m/qkp4se2d"
-    if st.button("Tương tác với Hình nón trên Geogebra"):
+    if st.button("Tìm hiểu các công thức của Hình nón trên Geogebra"):
         st.write("Mở liên kết [Geogebra - Hình nón](%s)" % geogebra_url)
         st.write("Hoặc bấm vào nút bên dưới để mở trong tab mới.")
         st.markdown(f'<a href="{geogebra_url}" target="_blank"><button>Geogebra - Hình nón</button></a>', unsafe_allow_html=True)
--- project/pages/page3.py
+++ project/pages/page3.py
@@ -7,7 +7,7 @@ def show():
     st.write("Hãy cùng tìm hiểu định nghĩa hình cầu qua hoạt động dưới đây.")
     
     geogebra_url = "https://www.geogebra.org/m/qkp4se2d"
-    if st.button("Tương tác với Hình cầu trên Geogebra"):
+    if st.button("Tìm hiểu định nghĩa Hình cầu trên Geogebra"):
         st.write("Mở liên kết [Geogebra - Hình cầu](%s)" % geogebra_url)
         st.write("Hoặc bấm vào nút bên dưới để mở trong tab mới.")
         st.markdown(f'<a href="{geogebra_url}" target="_blank"><button>Geogebra - Hình cầu</button></a>', unsafe_allow_html=True)
@@ -16,7 +16,7 @@ def show():
     st.write("Hãy cùng tìm hiểu công thức tính diện tích xung quanh và thể tích của hình cầu qua hoạt động dưới đây.")
 
     geogebra_url = "https://www.geogebra.org/m/qkp4se2d"
-    if st.button("Tương tác với Hình cầu trên Geogebra"):
+    if st.button("Tìm hiểu các công thức của Hình cầu trên Geogebra"):
         st.write("Mở liên kết [Geogebra - Hình cầu](%s)" % geogebra_url)
         st.write("Hoặc bấm vào nút bên dưới để mở trong tab mới.")
         st.markdown(f'<a href="{geogebra_url}" target="_blank"><button>Geogebra - Hình cầu</button></a>', unsafe_allow_html=True)
