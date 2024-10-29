135f24536a1af6a4d29c9dce47b0766c0004422e
MinTuanLuong <tuanlm.t1.1821@gmail.com>
2024-10-29 16:34:46 +0000

    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,41 @@
 import streamlit as st
 from project.pages import page1, page2, page3  # Import các trang phụ
 
-original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Streamlit CSS Styling✨ </h1>'
-st.markdown(original_title, unsafe_allow_html=True)
+# original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Streamlit CSS Styling✨ </h1>'
+# st.markdown(original_title, unsafe_allow_html=True)
 
 
-# Set the background image
-background_image = """
-<style>
-[data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
-    background-position: center;  
-    background-repeat: no-repeat;
-}
-</style>
-"""
+# # Set the background image
+# background_image = """
+# <style>
+# [data-testid="stAppViewContainer"] > .main {
+#     background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+#     background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+#     background-position: center;  
+#     background-repeat: no-repeat;
+# }
+# </style>
+# """
 
-st.markdown(background_image, unsafe_allow_html=True)
+# st.markdown(background_image, unsafe_allow_html=True)
 
-st.text_input("", placeholder="Streamlit CSS ")
+# st.text_input("", placeholder="Streamlit CSS ")
 
-input_style = """
-<style>
-input[type="text"] {
-    background-color: transparent;
-    color: #a19eae;  // This changes the text color inside the input box
-}
-div[data-baseweb="base-input"] {
-    background-color: transparent !important;
-}
-[data-testid="stAppViewContainer"] {
-    background-color: transparent !important;
-}
-</style>
-"""
-st.markdown(input_style, unsafe_allow_html=True)
+# input_style = """
+# <style>
+# input[type="text"] {
+#     background-color: transparent;
+#     color: #a19eae;  // This changes the text color inside the input box
+# }
+# div[data-baseweb="base-input"] {
+#     background-color: transparent !important;
+# }
+# [data-testid="stAppViewContainer"] {
+#     background-color: transparent !important;
+# }
+# </style>
+# """
+# st.markdown(input_style, unsafe_allow_html=True)
 
 # st.markdown(
 #     """
