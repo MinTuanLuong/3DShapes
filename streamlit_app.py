209c9ded887ff5f20ed55be5e7d0b063d9ece871
MinTuanLuong <tuanlm.t1.1821@gmail.com>
2024-10-29 16:30:10 +0000

    à

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,37 +5,51 @@ from project.pages import page1, page2, page3  # Import các trang phụ
 # st.markdown(original_title, unsafe_allow_html=True)
 
 
-# Set the background image
-background_image = """
-<style>
-[data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
-    background-position: center;  
-    background-repeat: no-repeat;
-}
-</style>
-"""
+# # Set the background image
+# background_image = """
+# <style>
+# [data-testid="stAppViewContainer"] > .main {
+#     background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+#     background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+#     background-position: center;  
+#     background-repeat: no-repeat;
+# }
+# </style>
+# """
 
-st.markdown(background_image, unsafe_allow_html=True)
+# st.markdown(background_image, unsafe_allow_html=True)
 
 # st.text_input("", placeholder="Streamlit CSS ")
 
-input_style = """
-<style>
-input[type="text"] {
-    background-color: transparent;
-    color: #a19eae;  // This changes the text color inside the input box
-}
-div[data-baseweb="base-input"] {
-    background-color: transparent !important;
-}
-[data-testid="stAppViewContainer"] {
-    background-color: transparent !important;
-}
-</style>
-"""
-st.markdown(input_style, unsafe_allow_html=True)
+# input_style = """
+# <style>
+# input[type="text"] {
+#     background-color: transparent;
+#     color: #a19eae;  // This changes the text color inside the input box
+# }
+# div[data-baseweb="base-input"] {
+#     background-color: transparent !important;
+# }
+# [data-testid="stAppViewContainer"] {
+#     background-color: transparent !important;
+# }
+# </style>
+# """
+# st.markdown(input_style, unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366")
+    }
+   .sidebar .sidebar-content {
+        background: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Hàm để tải nội dung từ file văn bản
 def load_file(file_path):
