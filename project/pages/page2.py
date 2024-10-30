68f289c523a20c0040e324c6efb16d8d0bfabcb3
MinTuanLuong <tuanlm.t1.1821@gmail.com>
2024-10-30 06:23:36 +0000

    a

--- project/pages/page2.py
+++ project/pages/page2.py
@@ -6,18 +6,18 @@ def show():
     st.header("Định nghĩa")
     st.write("Hãy cùng tìm hiểu định nghĩa hình nón qua hoạt động dưới đây.")
     
-    geogebra_url = "https://www.geogebra.org/m/qkp4se2d"
+    geogebra_url = "https://www.geogebra.org/m/yrk4srw2"
     if st.button("Tìm hiểu định nghĩa Hình nón trên Geogebra"):
-        st.write("Mở liên kết [Geogebra - Hình nón](%s)" % geogebra_url)
+        st.write("Mở liên kết [Geogebra - Định nghĩa hình nón](%s)" % geogebra_url)
         st.write("Hoặc bấm vào nút bên dưới để mở trong tab mới.")
         st.markdown(f'<a href="{geogebra_url}" target="_blank"><button>Geogebra - Hình nón</button></a>', unsafe_allow_html=True)
     
     st.header("Công thức")
     st.write("Hãy cùng tìm hiểu công thức tính diện tích xung quanh và thể tích của hình nón qua hoạt động dưới đây.")
 
-    geogebra_url = "https://www.geogebra.org/m/qkp4se2d"
+    geogebra_url = "https://www.geogebra.org/m/sbmwy2kt"
     if st.button("Tìm hiểu các công thức của Hình nón trên Geogebra"):
-        st.write("Mở liên kết [Geogebra - Hình nón](%s)" % geogebra_url)
+        st.write("Mở liên kết [Geogebra - Các công thức của hình nón](%s)" % geogebra_url)
         st.write("Hoặc bấm vào nút bên dưới để mở trong tab mới.")
         st.markdown(f'<a href="{geogebra_url}" target="_blank"><button>Geogebra - Hình nón</button></a>', unsafe_allow_html=True)
 
